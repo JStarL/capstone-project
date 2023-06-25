5bc6a54344989bcf05b22af2503422df3608c868
Jibitesh Saha (WSL) <z5280740@ad.unsw.edu.au>
2023-06-26 00:25:48 +0530

    switched kscode to lubuntu

--- database/test.py
+++ database/test.py
@@ -1,9 +1,11 @@
 import psycopg2
+import sys
 
+print (sys.argv[1])
 try:
     conn = psycopg2.connect('dbname=wait_management_system user=lubuntu password=lubuntu')
     print(conn)
     conn.close()
     print(conn)
 except Exception as e:
-    print( 'Unable to connect to database: ')
+    print( 'Unable to connect to database: ' + str(e))
