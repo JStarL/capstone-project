2f0553e60c31ac6cced7d8ad533ee166b96a5a07
Jibitesh Saha (WSL) <z5280740@ad.unsw.edu.au>
2023-06-25 17:57:11 +0530

    Added 6080 fetch_from_backend code

--- frontend/my-app/src/makeRequest.jsx
+++ frontend/my-app/src/makeRequest.jsx
@@ -1,5 +1,33 @@
 // import React from 'react';
 // import PropTypes from 'prop-types';
+const configData = require('../config.json');
+const BACKEND_BASE_URL = 'http://localhost:' + configData.BACKEND_PORT;
+
+export const fetchFromBackend = (subUrl, methodType, reqBody, token) => {
+  return new Promise((resolve, reject) => {
+    const reqHeaders = new Headers();
+
+    reqHeaders.set('accept', 'application/json');
+
+    if (reqBody !== undefined) {
+      reqHeaders.set('Content-Type', 'application/json');
+    }
+
+    if (!(subUrl.startsWith('/admin/auth/register') || subUrl.startsWith('/admin/auth/login'))) {
+      reqHeaders.set('Authorization', 'Bearer ' + token);
+    }
+
+    fetch(BACKEND_BASE_URL + subUrl, {
+      method: methodType,
+      headers: reqHeaders,
+      body: JSON.stringify(reqBody)
+    })
+      .then(res => res.json())
+      .then(val => resolve(val))
+      .catch(err => reject(err));
+  })
+};
+
 
 const makeRequest = async (route, method, body, token) => {
   let options = {};
