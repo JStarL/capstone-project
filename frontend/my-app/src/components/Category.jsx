bb793f602a24c47c5e5a1de24154045a8158d3dd
W Santoso <wsantoso@Ws-MacBook-Air.local>
2023-06-26 17:55:28 +1000

    fix category name updating with onBlur

--- frontend/my-app/src/components/Category.jsx
+++ frontend/my-app/src/components/Category.jsx
@@ -1,11 +1,13 @@
-import { Button, Typography, Card, CardActions, CardContent} from '@mui/material';
+import React from 'react';
+import { Button, TextField, Card, CardActions, CardContent} from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import MenuIcon from '@mui/icons-material/Menu';
 function Category() {
+	const [categoryName, setCategoryName] = React.useState('Category 1');
 	return <>
 		<Card onClick={() => console.log('Selecting category')} sx={{m:2, p:7}} variant="outlined" >
 			<CardContent>
-				<Typography> Category 1</Typography>
+				<TextField className='food-item-name' value={categoryName} onChange={e => setCategoryName(e.target.value)} onBlur={() => console.log(`Send request to backend to change name to ${categoryName}`)}label='Category Name'></TextField>
 			</CardContent>
 			<CardActions>
 				<Button startIcon={<DeleteIcon/>}></Button>			
