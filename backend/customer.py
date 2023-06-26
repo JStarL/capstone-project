e8f1d3ab8a708b5163516713d328f44366b5a149
Jibitesh Saha (WSL) <z5280740@ad.unsw.edu.au>
2023-06-26 08:09:18 +0530

    Reviewed and changed some of Rohan's code to the customer_view_menu function, removed class definition, included all food details in the return value for the Best Selling Category, {'category name': []} otherwise

--- backend/customer.py
+++ backend/customer.py
@@ -1,30 +1,41 @@
 
-class Customer():
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
+def customer_view_menu(cur, menu_id):
+    invalid_menu_id = { 'error': 'invalid menu id' } # error message
+    menu = []
 
-    def customer_view_menu(cur, session_id, menu_id):
-        invalid_session_id = { 'error': 'invalid session id' } # may combine  this error message with the thing below
-        invalid_menu_id = { 'error': 'invalid password' } # error message
-        menu = {}
+    query_categories = """
+    select id, name from categories where menu_id = %s order by id;
+    """
+    
+    query_menu_items = """
+    select id, title, description, image, price from menu_items where category_id = %s order by title;
+    """
+    cur.execute(query_categories, [menu_id])
+    categories = cur.fetchall()        
+    
+    if len(categories) == 0:
+        return invalid_menu_id
 
-        query_categories = """
-        select id, name from categories where menu_id = %s;
-        """
+    for categ in categories:
+        categ_id = categ[0]
         
-        query_food_items = """
-        select id from food_items where category_id = %s;
-        """
-        cur.execute(query_categories, [menu_id])
-        categories = cur.fetchall()        
+        if categ[1] == 'Best Selling':
+            # if this is the Best Selling Category,
+            # give the frontend all the information on
+            # food details so it can show on the UI
+
+            cur.execute(query_menu_items, [categ_id])
+            menu_items_list = []
+            for menu_item in cur.fetchall():
+                tmp = {}
+                tmp.update({'food_id': menu_item[0]})
+                tmp.update({'food_name': menu_item[1]})
+                tmp.update({'food_description': menu_item[2]})
+                tmp.update({'food_image': menu_item[3]})
+                tmp.update({'food_price': menu_item[4]})
+                menu_items_list.append(tmp)
+            menu.append({'Best Selling': menu_items_list})
+        else:
+            menu.append({categ[1]: []})
         
-        for categ in categories:
-            categ_id = categ['id']
-            cur.execute(query_food_items, [categ_id])
-            food_items = cur.fetchall()
-
-            food_item_ids = [food_item['id'] for food_item in food_items]
-            menu.update({categ['name'] : food_item_ids})
-
-        return invalid_session_id if len(categories) == 0 else menu
+    return menu
