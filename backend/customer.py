6a49215c1f12836f4601d18a13c3efba71b82eb5
Rohan <z5308712@ad.unsw.edu.au>
2023-06-26 21:41:02 +1000

    Added customer_view_menu implementation

--- backend/customer.py
+++ backend/customer.py
@@ -1,8 +1,21 @@
-from backend.staff import Staff
 
+class Customer():
+    def __init__(self, session_id, email, password):
+        self.session_id = session_id
+        self.email = email
+        self.password = password
 
-class Customer(Staff):
-    def __init__(self, id, email, password):
-        super().__init__(self, id, email, password)
+    def customer_view_menu(cur, session_id, menu_id):
+        invalid_session_id = { 'error': 'invalid session id' } # may combine  this error message with the thing below
+        invalid_menu_id = { 'error': 'invalid password' } # error message
+        menu = { 'success': 'Show menu' } # supposed to show the menu lol
 
-    def customer_view_menu():
+        query1 = """
+            
+        """ #empty for now as database hasn't been made yet
+        
+        cur.execute(query1, []) #empty for now
+        
+        list1 = cur.fetchall()
+        
+        return invalid_session_id if len(list1) == 0 else menu
