4d53e2ef5f97a5c9161ae7f44aa38aaff395a870
Jibitesh Saha (WSL) <z5280740@ad.unsw.edu.au>
2023-07-05 04:31:41 +0530

    Went through all the manager and customer routes to implement the side effects of having an ordering_id attribute on viewing the menu, the category, and the menu items

--- backend/customer.py
+++ backend/customer.py
@@ -4,11 +4,11 @@ def customer_view_menu(cur, menu_id):
     menu = []
 
     query_categories = """
-    select id, name from categories where menu_id = %s order by id;
+    select id, name, ordering_id from categories where menu_id = %s order by ordering_id;
     """
     
     query_menu_items = """
-    select id, title, description, image, price, ingredients from menu_items where category_id = %s order by title;
+    select id, title, description, image, price, ingredients, ordering_id from menu_items where category_id = %s order by ordering_id;
     """
     cur.execute(query_categories, [menu_id])
     categories = cur.fetchall()        
@@ -34,10 +34,11 @@ def customer_view_menu(cur, menu_id):
                 tmp.update({'food_image': menu_item[3]})
                 tmp.update({'food_price': menu_item[4]})
                 tmp.update({'food_ingredients': menu_item[5]})
+                tmp.update({'food_ordering_id': menu_item[6]})
                 menu_items_list.append(tmp)
-            menu.append({str(categ_id): ['Best Selling', menu_items_list]})
+            menu.append({str(categ_id): ['Best Selling', menu_items_list, categ[2]]})
         else:
-            menu.append({str(categ_id): [categ[1], []]})
+            menu.append({str(categ_id): [categ[1], [], categ[2]]})
         
     return menu
 
@@ -56,7 +57,7 @@ def customer_view_category(cur, category_id):
         return invalid_category_id
 
     query1 = """
-    select id, title, description, image, price, ingredients from menu_items where category_id = %s order by title;
+    select id, title, description, image, price, ingredients, ordering_id from menu_items where category_id = %s order by ordering_id;
     """
 
     cur.execute(query1, [category_id])
@@ -70,6 +71,7 @@ def customer_view_category(cur, category_id):
         tmp.update({'food_image': tup[3]})
         tmp.update({'food_price': tup[4]})
         tmp.update({'food_ingredients': tup[5]})
+        tmp.update({'food_ordering_id': tup[6]})
         menu_items.append(tmp)
 
     return menu_items
@@ -80,7 +82,7 @@ def customer_view_menu_item(cur, menu_item_id):
     food = { 'success': 'Show food' } # supposed to show the food lol
     
     query1 = """
-    select title, description, image, price, ingredients, category_id from menu_items where id = %s;
+    select title, description, image, price, ingredients, category_id, ordering_id from menu_items where id = %s;
     """ 
     
     cur.execute(query1, [menu_item_id])
@@ -99,6 +101,7 @@ def customer_view_menu_item(cur, menu_item_id):
         food.update({'food_price': tup[3]})
         food.update({'food_ingredients': tup[4]})
         food.update({'category_id': tup[5]})
+        food.update({'food_ordering_id': tup[6]})
         return food
     
 def customer_menu_search(cur, query):
