2b7c4830d278dddc1cf131180c5c969d6c7f0180
W Santoso <wsantoso@Ws-MacBook-Air.local>
2023-07-06 14:06:46 +1000

    Improve style for individual food item

--- frontend/my-app/src/pages/FoodItemPage.jsx
+++ frontend/my-app/src/pages/FoodItemPage.jsx
@@ -1,56 +1,82 @@
 import React from 'react';
 import '../App.css';
-import { Button, TextField, Typography, Paper } from '@mui/material';
+import { Button, TextField, Typography, Paper, Box } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useNavigate, useParams } from 'react-router-dom';
 import makeRequest from '../makeRequest';
+import { StyledButton } from './CustomerOrStaff';
 
 function FoodItemPage() {
-	const navigate = useNavigate();
-	const sessionId = localStorage.getItem('session_id');
-	const [foodData, setFoodData] = React.useState({}); 
-	const params = useParams()
-	const [foodId, setFoodId] = React.useState(params.foodId); 
+  const navigate = useNavigate();
+  const sessionId = localStorage.getItem('session_id');
+  const [foodData, setFoodData] = React.useState({});
+  const params = useParams();
+  const [foodId, setFoodId] = React.useState(params.foodId);
 
-	React.useEffect(() => {
-		setFoodId(params.foodId)
-		fetchFoodItemData()
-	}, [foodId]);
+  React.useEffect(() => {
+    setFoodId(params.foodId);
+    fetchFoodItemData();
+  }, [foodId]);
 
-	async function fetchFoodItemData() {
-		// CHANGE URL WHEN MENU ID is stored after searching a restaurant
-		const url = `/customer/view_menu_item?session_id=${sessionId}&menu_item_id=${foodId}`;
-		const data = await makeRequest(url, 'GET', undefined, undefined)
-		console.log(data)
-		setFoodData(data)
-	}
-	if (!foodData) return <>loading...</>;
+  async function fetchFoodItemData() {
+    const url = `/customer/view_menu_item?session_id=${sessionId}&menu_item_id=${foodId}`;
+    const data = await makeRequest(url, 'GET', undefined, undefined);
+    console.log(data);
+    setFoodData(data);
+  }
 
-	return <>
-		<div className='login-page'>
-			{/* change the navigate later when customer route is more dynamic */}
-			<Button onClick={() => navigate('/customer/1')} startIcon={<ArrowBackIcon size='large' />}>Go Back</Button>
-			<Paper className='paper' elevation={3} sx={{ p: 8, mb: 2}}>
+  if (!foodData) return <>loading...</>;
 
-				<Typography className='h4' variant="h4" gutterBottom>{foodData.food_name}</Typography>
-				<div className='food-item-div'>
-					<div>
-						{foodData.image
-							? <div className='image'><img style={{ height: '200px', width: '200px', margin: '5px' }} src={foodData.food_image}></img></div>
-							: <div className='food-item-img'>IMG</div>}
-					</div>
-					<div className='food-item-middle'>
-						<div className='div-section'>Ingredients: {foodData.food_ingredients}</div>
-						<div className='div-section'>Description: {foodData.food_description}</div>
-						<div className='div-section'><b>Price: $ {foodData.food_price}</b></div>
-					</div>
-					<div className='food-item-button'>
-						<Button>Add to Order</Button>
-					</div>
-				</div>
-			</Paper>
-		</div>
-	</>
+  return (
+    <>
+      <div className='login-page'>
+        <Paper className='paper' elevation={5} sx={{ p: 4, mb: 2, position: 'relative', borderRadius:'20px'}}>
+          <StyledButton variant='outlined' onClick={() => navigate('/customer/1')} startIcon={<ArrowBackIcon size='large' />} sx={{ position: 'absolute', top: '16px', left: '16px', width:'auto' }}>
+            menu
+          </StyledButton>
+          <Typography className='h4' variant='h4' gutterBottom>
+            {foodData.food_name}
+          </Typography>
+          {foodData.food_image ? (
+            <img style={{ width: '100%', height: 'auto', margin: '5px' }} src={foodData.food_image} alt='Food Item' />
+          ) : (
+            <div style={{ width: '100%', height: 'auto', margin: '5px' }} className='food-item-img'>
+              FOOD IMG
+            </div>
+          )}
+          <div className='div-section'>
+            <Typography variant='subtitle1' fontWeight='bold'>
+              Ingredients:
+            </Typography>
+            <Typography>
+              {foodData.food_ingredients}
+            </Typography>
+          </div>
+          <div className='div-section'>
+            <Typography variant='subtitle1' fontWeight='bold'>
+              Description:
+            </Typography>
+            <Typography>
+              {foodData.food_description}
+            </Typography>
+          </div>
+          <div className='div-section'>
+            <Typography variant='subtitle1' fontWeight='bold'>
+              Price:
+            </Typography>
+            <Typography>
+              $ {foodData.food_price}
+            </Typography>
+          </div>
+          <Box mt={2}>
+            <StyledButton variant='outlined'>
+              Add to Order
+            </StyledButton>
+          </Box>
+        </Paper>
+      </div>
+    </>
+  );
 }
 
 export default FoodItemPage;
