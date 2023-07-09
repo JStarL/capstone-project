a0de6a010357c7b139cecd772812c23a3a989553
W Santoso <wsantoso@Ws-MacBook-Air.local>
2023-07-09 14:21:57 +1000

    Working on personalise allergy checkboxes

--- frontend/my-app/src/pages/PersonalisePage.jsx
+++ frontend/my-app/src/pages/PersonalisePage.jsx
@@ -1,40 +1,83 @@
 import React from 'react';
-import { Typography, Paper } from '@mui/material';
+import {
+  Typography,
+  Paper,
+  Checkbox,
+  FormControlLabel,
+  Button,
+} from '@mui/material';
 import { useNavigate, useParams } from 'react-router-dom';
 import makeRequest from '../makeRequest';
 
 function PersonalisePage() {
-	const [allergies, setAllergies] = React.useState([]);
+  const [allergies, setAllergies] = React.useState([]);
+  const [selectedAllergies, setSelectedAllergies] = React.useState([]);
 
-	React.useEffect(() => {
-		const fetchData = async () => {
-			const data = await fetchAllergies();
-			console.log(data)
-		};
-		fetchData();
-	}, []);
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchAllergies();
+      console.log(data);
+    };
+    fetchData();
+  }, []);
 
-	async function fetchAllergies() {
-		const url = `/customer/get_allergies?session_id=${localStorage.getItem('session_id')}`;
-		const data = await makeRequest(url, 'GET', undefined, undefined);
-		setAllergies(data)
-		return data;
-	}
+  async function fetchAllergies() {
+    const url = '/get_allergies';
+    const data = await makeRequest(url, 'GET', undefined, undefined);
+    setAllergies(data);
+    return data;
+  }
 
-	return (
-		<>
-			<div className='login-page' sx={{ alignItems: 'center' }}>
-			<Paper elevation={10} sx={{ p: 6, borderRadius: '20px', width: '60%' }}>
-				<Typography sx={{ mb: 3 }} variant="h4" gutterBottom>Personalise</Typography>
-				<div>
-					{allergies?.map((allergy) => (
-						console.log(allergy)
-					))}
-				</div>
-			</Paper>
-		</div>
-		</>
-	)
+  const handleCheckboxChange = (event) => {
+    const { value, checked } = event.target;
+    if (checked) {
+      setSelectedAllergies((prevSelectedAllergies) => [
+        ...prevSelectedAllergies,
+        value,
+      ]);
+    } else {
+      setSelectedAllergies((prevSelectedAllergies) =>
+        prevSelectedAllergies.filter((allergy) => allergy !== value)
+      );
+    }
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(selectedAllergies);
+  };
+
+  return (
+    <>
+      <div className="login-page" sx={{ alignItems: 'center' }}>
+        <Paper elevation={10} sx={{ p: 6, borderRadius: '20px', width: '60%' }}>
+          <Typography sx={{ mb: 3 }} variant="h4" gutterBottom>
+            Personalise
+          </Typography>
+          <form onSubmit={handleFormSubmit}>
+            <div>
+              {allergies?.map((allergy) => (
+                <FormControlLabel
+                  key={allergy[0]}
+                  control={
+                    <Checkbox
+                      onChange={handleCheckboxChange}
+                      value={allergy[0]}
+                    />
+                  }
+                  label={`${allergy[1]}: ${allergy[2]}`}
+                  value={allergy[0]}
+                />
+              ))}
+            </div>
+            <Button type="submit" variant="contained" color="primary">
+              Submit
+            </Button>
+          </form>
+        </Paper>
+      </div>
+    </>
+  );
 }
 
 export default PersonalisePage;
