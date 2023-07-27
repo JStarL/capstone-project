efd100d17cf1b0d2878d22e8d8772a99caec28c2
W Santoso <wsantoso@Ws-MacBook-Air.local>
2023-07-28 00:47:10 +1000

    Small change to customerPayPage

--- frontend/my-app/src/pages/CustomerPayPage.jsx
+++ frontend/my-app/src/pages/CustomerPayPage.jsx
@@ -3,11 +3,12 @@ import '../App.css';
 import { TextField, Typography, Paper, FormLabel, FormControl, Radio, RadioGroup, FormControlLabel, Snackbar, Alert } from '@mui/material';
 import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
 import { StyledRadio } from '../components/NewStaffForm';
+import { useParams } from 'react-router-dom';
 
 function CustomerPayPage() {
-	const tableNumber = localStorage.getItem('table_number')
 	const [paymentType, setPaymentType] = React.useState('group')
-
+	const params = useParams()
+	const tableId = params.tableNumber
 	return (
 		<div className='login-page' sx={{ alignItems: 'center' }}>
 			<Paper elevation={10} sx={{ p: 6, borderRadius: '20px', width: '50%' }}>
@@ -15,7 +16,7 @@ function CustomerPayPage() {
 					My Bill <ReceiptLongIcon sx={{ fontSize: '4rem', verticalAlign: 'middle', marginBottom: '10px'}} />
 				</Typography>
 				<Typography sx={{ mb: 3 }} variant="h5" gutterBottom>Please go to the front counter to pay</Typography>
-				<Typography sx={{ mb: 3 }} variant="h5" gutterBottom><b>Table Number:</b> {tableNumber}</Typography>
+				<Typography sx={{ mb: 3 }} variant="h5" gutterBottom><b>Table Number:</b> {tableId}</Typography>
 				<FormControl>
 					<FormLabel id="payment-type">
 						<Typography sx={{ mb: 3, fontWeight: 'bold' }} variant="h5" gutterBottom>Payment Options:</Typography>
