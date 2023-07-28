c35628c3a1fbd8113d3097a6d3af289c1dec850e
W Santoso <wsantoso@Ws-MacBook-Air.local>
2023-07-28 13:20:57 +1000

    added notifcation bar when customer requests assistance

--- frontend/my-app/src/components/Footer.jsx
+++ frontend/my-app/src/components/Footer.jsx
@@ -8,9 +8,11 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import { StyledButton } from '../pages/CustomerOrStaff';
 import makeRequest from '../makeRequest';
 import BackHandIcon from '@mui/icons-material/BackHand';
+import { Typography, Button, Snackbar, Alert } from '@mui/material';
 
+function Footer(props) {
+  const [isSnackbarOpen, setSnackbarOpen] = React.useState(false); // State variable for Snackbar open/close status
 
-function Footer (props) {
   const requestAssistance = () => {
     const body = JSON.stringify({
       table_id: props.tableNumber,
@@ -20,9 +22,15 @@ function Footer (props) {
     makeRequest('/customer/request_assistance', 'POST', body, undefined)
       .then(data => {
         console.log(data);
+        setSnackbarOpen(true); // Open the Snackbar on successful request
       })
       .catch(e => console.log('Error: ' + e));
   }
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false); // Close the Snackbar
+  };
+
   const Footer = () => {
     if (props.isManager) {
       return (
@@ -50,6 +58,22 @@ function Footer (props) {
           <StyledButton onClick={requestAssistance} startIcon={<BackHandIcon />} className="toNavy" >
             Request Assistance
           </StyledButton>
+          <Snackbar
+            sx={{
+              width: '50%',
+              '& .MuiSnackbarContent-root': {
+                fontSize: '1.2rem',
+              },
+            }}
+            open={isSnackbarOpen}
+            autoHideDuration={4000}
+            onClose={handleSnackbarClose}
+            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+          >
+            <Alert onClose={handleSnackbarClose} severity="success" sx={{ fontSize: '2rem', width: 'auto' }}>
+              Request for assistance sent!
+            </Alert>
+          </Snackbar>
         </div>
       );
     } else {
