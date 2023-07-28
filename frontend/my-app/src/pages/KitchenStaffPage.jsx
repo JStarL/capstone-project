676d021630490639ec4fc56429996571e098f575
Hannah Park <hannahpark@Hannahs-MacBook-Pro-3.local>
2023-07-28 17:34:03 +1000

    set interval added to kitchen staff and wait staff

--- frontend/my-app/src/pages/KitchenStaffPage.jsx
+++ frontend/my-app/src/pages/KitchenStaffPage.jsx
@@ -31,14 +31,14 @@ function KitchenStaffPage() {
     fetchData();
 
     // Setup an interval to call fetchData every 3 seconds
-    // const interval = setInterval(() => {
-    //   fetchData();
-    // }, 3000);
+    const interval = setInterval(() => {
+      fetchData();
+    }, 3000);
 
-    // // Clear the interval when the component unmounts
-    // return () => {
-    //   clearInterval(interval);
-    // };
+    // Clear the interval when the component unmounts
+    return () => {
+      clearInterval(interval);
+    };
   }, [trigger]);
 
   async function getOrderList() {
