2121bc22b45e34a62e92733e35fbe25ead07b555
z5316427 <z5316427@vx07>
2023-08-02 02:18:23 +1000

    adding some comments and deleting some

--- backend/server.py
+++ backend/server.py
@@ -209,30 +209,37 @@ notifications = {}
 #
 #
 
-
-
-# @APP.route("/echo", methods=['GET'])
-# def echo():
-#     data = request.args.get('data')
-#     return dumps({
-#         'data': echo_v2(data)
-#     })
-
-
-# @APP.route('/clear/v1', methods=['DELETE'])
-# def clear():
-#     return dumps(clear_v1())
-
 @APP.errorhandler(400)
 def error_page(error):
     return {'error': error.description}, 400
     
-
 # Auth functions
-
 
 @APP.route('/auth/register', methods=['POST'])
 def register_flask():
+    """
+    Gets the input from the frontend and then sends that data to be processed
+    and registers the manager to the database. It also adds the menu and the best selling category to the resturant
+    
+    Inputs:
+        - email (string): The email
+        - password (string): The password
+        - name (string): The name
+        - restaurant_name (string): The restaurant name
+        - location (string): The location of the restaurant
+
+    Returns:
+        - return_val: {
+            'success': string
+            'manager_id': string
+            'menu_id': string
+            'category': string
+        }
+        OR
+        - return_value: {
+            'error': string
+        }
+    """
     data = ast.literal_eval(request.get_json())
     cur = db_conn.cursor()
     return_dict = register_backend(cur, data['email'], data['password'], data['name'], data['restaurant_name'], data['location'])
@@ -1153,286 +1160,6 @@ def wait_staff_mark_currently_assisting_flask():
             return dumps(success)
 
     return abort(400, not_found)
-
-##############################################################################################################################
-################################################ OLD PROJECT STUFF ###########################################################
-##############################################################################################################################
-
-# Channels functions
-'''
-
-@APP.route("/channels/create/v2", methods=['POST'])
-def server_channels_create_v2():
-    data = request.get_json()
-    return dumps(channels_create_v2(data['token'], data['name'], data['is_public']))
-
-
-@APP.route("/channels/list/v2", methods=['GET'])
-def server_channels_list_v2():
-    token = request.args.get("token")
-    return dumps(channels_list_v2(token))
-
-
-@APP.route("/channels/listall/v2", methods=['GET'])
-def server_channels_listall_return():
-    token = request.args.get("token")
-    return dumps(channels_listall_v2(token))
-
-
-# Channel functions
-
-
-@APP.route("/channel/details/v2", methods=['GET'])
-def server_channels_detail_return():
-    token = request.args.get("token")
-    channel_id = int(request.args.get("channel_id"))
-    return dumps(channel_details_v2(token, channel_id))
-
-
-@APP.route("/channel/join/v2", methods=['POST'])
-def server_channel_join_v2():
-    data = request.get_json()
-    channel_join_v2(data['token'], data['channel_id'])
-    return {}
-
-
-@APP.route("/channel/invite/v2", methods=['POST'])
-def server_channel_invite_v2():
-    data = request.get_json()
-    channel_invite_v2(data['token'], data['channel_id'], data['u_id'])
-    return {}
-
-
-@APP.route("/channel/messages/v2", methods=['GET'])
-def server_channel_messages_v2():
-    token = request.args.get('token')
-    channel_id = int(request.args.get('channel_id'))
-    start = int(request.args.get('start'))
-    return dumps(channel_messages_v2(token, channel_id, start))
-
-
-@APP.route("/channel/leave/v1", methods=['POST'])
-def server_channel_leave_v1():
-    data = request.get_json()
-    return dumps(channel_leave_v1(data['token'], data['channel_id']))
-
-
-@APP.route("/channel/addowner/v1", methods=['POST'])
-def server_channels_addowner_v1():
-    data = request.get_json()
-    return dumps(channel_addowner_v1(data['token'], data['channel_id'], data['u_id']))
-
-
-@APP.route("/channel/removeowner/v1", methods=['POST'])
-def server_channels_removeowner_v1():
-    data = request.get_json()
-    return dumps(channel_removeowner_v1(data['token'], data['channel_id'], data['u_id']))
-
-
-# DM functions
-
-
-@APP.route("/dm/create/v1", methods=['POST'])
-def server_dm_create_v1():
-    data = request.get_json()
-    return dumps(dm_create_v1(data['token'], data['u_ids']))
-
-
-@APP.route("/dm/list/v1", methods=['GET'])
-def server_dm_list_v1():
-    token = request.args.get('token')
-    return dumps(dm_list_v1(token))
-
-
-@APP.route("/dm/remove/v1", methods=['DELETE'])
-def server_dm_remove_v1():
-    data = request.get_json()
-    dm_remove_v1(data['token'], data['dm_id'])
-    return {}
-
-
-@APP.route("/dm/details/v1", methods=['GET'])
-def server_dm_details_v1():
-    token = request.args.get('token')
-    dm_id = int(request.args.get('dm_id'))
-    return dumps(dm_details_v1(token, dm_id))
-
-
-@APP.route("/dm/leave/v1", methods=['POST'])
-def server_dm_leave_v1():
-    data = request.get_json()
-    dm_leave_v1(data['token'], data['dm_id'])
-    return {}
-
-
-@APP.route("/dm/messages/v1", methods=['GET'])
-def server_dm_messages_v1():
-    token = request.args.get('token')
-    dm_id = int(request.args.get('dm_id'))
-    start = int(request.args.get('start'))
-    return dumps(dm_messages_v1(token, dm_id, start))
-
-
-# Message functions
-
-
-@APP.route("/message/send/v1", methods=['POST'])
-def server_message_send_v1():
-    data = request.get_json()
-    return dumps(message_send_v1(data['token'], data['channel_id'], data['message']))
-
-
-@APP.route("/message/edit/v1", methods=['PUT'])
-def server_message_edit_v1():
-    data = request.get_json()
-    message_edit_v1(data['token'], data['message_id'], data['message'])
-    return dumps({})
-
-
-@APP.route("/message/remove/v1", methods=['DELETE'])
-def server_message_remove_v1():
-    data = request.get_json()
-    message_remove_v1(data['token'], data['message_id'])
-    return dumps({})
-
-
-@APP.route("/message/senddm/v1", methods=['POST'])
-def server_message_senddm_v1():
-    data = request.get_json()
-    return dumps(message_senddm_v1(data['token'], data['dm_id'], data['message']))
-
-@APP.route("/message/share/v1", methods=['POST'])
-def server_message_share_v1():
-    data = request.get_json()
-    return dumps(message_share_v1(data['token'], data['og_message_id'], data['message'], data['channel_id'], data['dm_id']))
-
-@APP.route("/message/react/v1", methods = ['POST'])
-def server_message_react_v1():
-    data = request.get_json()
-    return dumps(message_react_v1(data['token'], data['message_id'], data['react_id']))
-
-@APP.route("/message/unreact/v1", methods = ['POST'])
-def server_message_unreact_v1():
-    data = request.get_json()
-    return dumps(message_unreact_v1(data['token'], data['message_id'], data['react_id']))
-
-@APP.route("/message/pin/v1", methods = ['POST'])
-def server_message_pin_v1():
-    data = request.get_json()
-    return dumps(message_pin_v1(data['token'], data['message_id']))
-
-@APP.route("/message/unpin/v1", methods = ['POST'])
-def server_message_unpin_v1():
-    data = request.get_json()
-    return dumps(message_unpin_v1(data['token'], data['message_id']))
-
-
-@APP.route('/message/sendlater/v1', methods=['POST'])
-def message_sendlater():
-    data = request.get_json()
-    return dumps(message_sendlater_v1(data['token'], data['channel_id'], data['message'], data['time_sent']))
-
-
-@APP.route('/message/sendlaterdm/v1', methods=['POST'])
-def message_sendlaterdm():
-    data = request.get_json()
-    return dumps(message_sendlaterdm_v1(data['token'], data['dm_id'], data['message'], data['time_sent']))
-    
-    
-# Profile Functions requests
-
-
-@APP.route('/users/all/v1', methods=['GET'])
-def users_all():
-    token = request.args.get('token')
-    return dumps(users_all_v1(token))
-
-
-@APP.route("/user/profile/v1", methods=['GET'])
-def user_profile_return():
-    token = request.args.get('token')
-    u_id = int(request.args.get('u_id'))
-    return dumps(user_profile_v1(token, u_id))
-
-
-@APP.route("/user/profile/setname/v1", methods=['PUT'])
-def user_profile_setname():
-    data = request.get_json()
-    return dumps(user_profile_setname_v1(data['token'], data['name_first'], data['name_last']))
-
-
-@APP.route("/user/profile/setemail/v1", methods=['PUT'])
-def user_profile_setemail():
-    data = request.get_json()
-    return dumps(user_profile_setemail_v1(data['token'], data['email']))
-
-
-@APP.route("/user/profile/sethandle/v1", methods=['PUT'])
-def user_profile_sethandle():
-    data = request.get_json()
-    return dumps(user_profile_sethandle_v1(data['token'], data['handle_str']))
-
-@APP.route("/user/profile/uploadphoto/v1", methods=['POST'])
-def user_profile_uploadingphoto():
-    data = request.get_json()
-    return dumps(user_profile_uploadphoto_v1(data['token'], data['img_url'], data['x_start'], data['y_start'], data['x_end'], data['y_end']))
-
-@APP.route("/user/stats/v1", methods=['GET'])
-def user_stats():
-    token = request.args.get('token')
-    return dumps(user_stats_v1(token))
-
-@APP.route("/users/stats/v1", methods=['GET'])
-def users_stats():
-    token = request.args.get('token')
-    return dumps(users_stats_v1(token))
-
-
-# Admin functions
-@APP.route('/admin/userpermission/change/v1', methods=['POST'])
-def admin_userpermission_change():
-    data = request.get_json()
-    return dumps(admin_userpermission_change_v1(data['token'], data['u_id'], data['permission_id']))
-
-
-@APP.route('/admin/user/remove/v1', methods=['DELETE'])
-def admin_user_remove():
-    data = request.get_json()
-    return dumps(admin_user_remove_v1(data['token'], data['u_id']))
-
-# Standup functions
-
-
-@APP.route("/standup/start/v1", methods=['POST'])
-def server_standup_start_v1():
-    data = request.get_json()
-    return dumps(standup_start_v1(data['token'], data['channel_id'], data['length']))
-
-@APP.route("/standup/send/v1", methods=['POST'])
-def server_standup_send_v1():
-    data = request.get_json()
-    return dumps(standup_send_v1(data['token'], data['channel_id'], data['message']))
-
-@APP.route("/standup/active/v1", methods=['GET'])
-def server_standup_active_v1():
-    token = request.args.get('token')
-    channel_id = int(request.args.get('channel_id'))
-    return dumps(standup_active_v1(token, channel_id))
-
-@APP.route("/search/v1", methods=['GET'])
-def server_search_v1():
-    token = request.args.get('token')
-    query_str = str(request.args.get('query_str'))
-    return dumps(search_v1(token, query_str))
-
-
-# Notifications
-
-@APP.route('/notifications/get/v1', methods=['GET'])
-def server_notifications_get_v1():
-    token = request.args.get('token')
-    return dumps(notifications_get_v1(token))
-'''
 
 # NO NEED TO MODIFY BELOW THIS POINT
 
