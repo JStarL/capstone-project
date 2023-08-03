1f37d7f1f1d83ab1b827d25159c752b9535ea1e3
Hannah Park <hannahpark@Hannahs-MacBook-Pro-3.local>
2023-08-03 11:39:35 +1000

    javadoc added

--- frontend/my-app/src/components/CategoryManager.jsx
+++ frontend/my-app/src/components/CategoryManager.jsx
@@ -7,56 +7,72 @@ import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import makeRequest from '../makeRequest';
 
-// CategoryManager component takes care of displaying and managing a single category.
+/**
+ * Component for managing individual categories in a menu.
+ *
+ * @param {Object} props - Component props
+ * @param {string} props.categoryName - The name of the category.
+ * @param {number} props.id - The unique identifier of the category.
+ * @param {number} props.currentSelectedCategoryId - The currently selected category's ID.
+ * @param {Function} props.setCurrentSelectedCategoryId - Function to set the currently selected category's ID.
+ * @param {string} props.currentSelectedCategory - The name of the currently selected category.
+ * @param {Function} props.setCurrentSelectedCategory - Function to set the currently selected category's name.
+ * @param {Function} props.fetchAllMenuData - Function to fetch all menu data.
+ * @param {number} props.index - The index of the category in the list.
+ * @param {number} props.categoriesSize - The total number of categories in the list.
+ * @param {number} props.orderingId - The ordering ID of the category.
+ * @param {Function} props.getOtherCategoryOrderingId - Function to get the ordering ID of another category.
+ *
+ * @returns {JSX.Element} - The JSX element representing the CategoryManager component.
+ */
 function CategoryManager(props) {
-  // State variables to manage the category name and id.
   const [categoryName, setCategoryName] = React.useState(props.categoryName);
   const [categoryId] = React.useState(props.id);
-
-  // Get route parameters using useParams from react-router-dom.
   const params = useParams();
   const managerId = params.managerId;
   const menuId = params.menuId;
 
-  // Function to delete the current category.
+  /**
+   * Deletes the current category.
+   */
   function deleteCategory() {
-    // Prepare the request body with necessary data.
     const body = JSON.stringify({
       manager_id: managerId,
       menu_id: menuId,
       category_id: props.id
     });
 
-    // Send a request to the server to delete the category.
     makeRequest('/manager/delete_category', 'DELETE', body, undefined)
       .then(data => {
         if (props.currentSelectedCategoryId === categoryId) {
-          // If the deleted category was selected, change the selected category to 'Best Selling'.
+          // Deleting the currently selected category should automatically change selected category to "Best Selling".
           props.setCurrentSelectedCategoryId(1);
           props.setCurrentSelectedCategory('Best Selling');
         }
-        // Fetch updated menu data after deletion.
         props.fetchAllMenuData();
       })
       .catch(e => console.log('Error: ' + e));
   }
 
-  // Function to reorder the category.
+  /**
+   * Reorders the current category.
+   *
+   * @param {number} prev_ordering_id - The previous ordering ID of the category.
+   * @param {number} new_ordering_id - The new ordering ID of the category.
+   */
   function reorderCategory(prev_ordering_id, new_ordering_id) {
     if (!new_ordering_id) {
       return;
     }
     const body = JSON.stringify({
       manager_id: managerId,
-      category_id: props.id, 
-      prev_ordering_id, 
+      category_id: props.id,
+      prev_ordering_id,
       new_ordering_id
     });
     if (categoryName !== '') {
-      // Send a request to the server to update the category's ordering.
       makeRequest('/manager/update_category_ordering', 'POST', body, undefined)
         .then(data => {
-          // Fetch updated menu data after reordering.
           props.fetchAllMenuData();
         })
         .catch(e => console.log('Error: ' + e));
@@ -65,7 +81,9 @@ function CategoryManager(props) {
     }
   }
 
-  // Function to update the category name.
+  /**
+   * Updates the name of the current category.
+   */
   function updateCategoryName() {
     const body = JSON.stringify({
       manager_id: managerId,
@@ -73,10 +91,8 @@ function CategoryManager(props) {
       category_id: props.id
     });
     if (categoryName !== '') {
-      // Send a request to the server to update the category's name.
       makeRequest('/manager/update_category', 'POST', body, undefined)
         .then(data => {
-          // Fetch updated menu data after updating the name.
           props.fetchAllMenuData();
         })
         .catch(e => console.log('Error: ' + e));
@@ -89,14 +105,14 @@ function CategoryManager(props) {
     }
   }
 
-  // Function to select the current category.
+  /**
+   * Selects the current category.
+   */
   function selectCategory() {
-    // Set the selected category name and id in the parent component's state.
     props.setCurrentSelectedCategory(props.categoryName);
     props.setCurrentSelectedCategoryId(categoryId);
   }
 
-  // Render the CategoryManager component.
   return (
     <Box display="flex" alignItems="center">
       <Card
@@ -112,25 +128,24 @@ function CategoryManager(props) {
         sx={{ m: 2, p: 7 }}
         variant="outlined"
       >
-  			<CardContent style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+        <CardContent style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
           {categoryName === 'Best Selling' ? (
-				    <Typography style={{ fontSize:'15px' }} variant='overline'><b>Best Selling</b></Typography>
+            <Typography style={{ fontSize: '15px' }} variant='overline'><b>Best Selling</b></Typography>
           ) : (
-            <TextField
-              fullWidth
-              variant="standard"
-              className="food-item-name"
-              value={categoryName}
-              onChange={e => setCategoryName(e.target.value)}
-              onBlur={() => updateCategoryName()}
-              label="Category Name"
-            ></TextField>
-          )}
+              <TextField
+                fullWidth
+                variant="standard"
+                className="food-item-name"
+                value={categoryName}
+                onChange={e => setCategoryName(e.target.value)}
+                onBlur={() => updateCategoryName()}
+                label="Category Name"
+              ></TextField>
+            )}
         </CardContent>
       </Card>
       {categoryName === 'Best Selling' ? <div style={{ width: "55px" }}></div> : (
         <Box style={{ width: "55px" }} display="flex" justifyContent='center' flexDirection="column">
-          {/* Buttons to reorder and delete the category */}
           <Button disabled={props.index === 1} sx={{ color: '#002250' }} onClick={() => reorderCategory(props.orderingId, props.getOtherCategoryOrderingId('up', categoryId))} startIcon={<ArrowUpwardIcon />} />
           <Button sx={{ color: '#002250' }} onClick={() => deleteCategory()} startIcon={<DeleteIcon />} />
           <Button disabled={props.index === props.categoriesSize - 1} sx={{ color: '#002250' }} onClick={() => reorderCategory(props.orderingId, props.getOtherCategoryOrderingId('down', categoryId))} startIcon={<ArrowDownwardIcon />} />
